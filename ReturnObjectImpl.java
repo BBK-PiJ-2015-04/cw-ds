99bbdc82086ea17cb1ba040e0fc8acc4e27f72d1
HS <none@email.none>
2015-12-15 13:40:45 +0000

    .

---
+++ ReturnObjectImpl.java
@@ -0,0 +1,15 @@
+public class ReturnObjectImpl implements ReturnObject {
+	
+	public boolean hasError() {
+		
+	}
+
+	public ErrorMessage getError() {
+		
+	}
+
+	public Object getReturnValue() {
+		
+	}
+
+}
